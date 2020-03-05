fc65f929ca0ecb2f4e2d9c72bd013524271e8429
agademic <arthur.gogohia@gmail.com>
2020-03-05 23:17:40 +0100

    baselines + missingness percent added + random seed set to 8

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Created on Mon Feb 10 17:11:29 2020
 """
 
 from pathlib import Path
+import numpy as np
 
 #import numpy as np
 #import pandas as pd
@@ -44,6 +45,7 @@ and test set.
 7. Run model data preparation and training file.
 
 """
+np.random.seed(8)
 
 # dataset = load_data(load_boston)
 # dataset = load_boston()
@@ -57,52 +59,60 @@ and test set.
 # t, t1 = generate_missingness(dataset, mask)
 
 def data_setup():
-    
+    imputation_scores = []
     # load real world datassets
     for name, loader in dataset_dict.items():
         dataset = load_data(loader, False)
         print(name)
         for miss_type in missingness['missingness_type']:
             print(miss_type)
-            mask = generate_missing_mask(dataset, missingness=miss_type)
-            training_set, test_set = generate_missingness(dataset, mask)
-            # write training set
-            source_factors = create_source_factors(training_set)
-            source, target = convert_to_strings(training_set)
-            source, target, source_factors = shuffle_data(source, target, source_factors)
-            #file_name = str(name + '/' + miss_type)
-            file_name = Path('data/' + str(name + '/' + miss_type))
-            create_files(source, target, source_factors, file_dir=file_name, set_type='train')
-            # write testing set
-            source_factors = create_source_factors(test_set)
-            source, target = convert_to_strings(test_set)
-            create_files(source, target, source_factors, file_dir=file_name, set_type='test')
-            # compute mse using imputation methods and save file
-            imputation_scores = sklearn_impute(training_set, test_set)
-            imputation_scores.to_pickle(Path(file_name.__str__() + 'imputation_score.pkl'))
-    
+            for miss_percent in missingness['missingness_percent']:
+                print(miss_percent)
+                mask = generate_missing_mask(dataset, missingness=miss_type)
+                training_set, test_set = generate_missingness(dataset, mask)
+                # write training set
+                source_factors = create_source_factors(training_set)
+                source, target = convert_to_strings(training_set)
+                source, target, source_factors = shuffle_data(source, target, source_factors)
+                #file_name = str(name + '/' + miss_type)
+                file_name = Path('data/' + f'{name}/{miss_type}/{miss_percent}')
+                create_files(source, target, source_factors, file_dir=file_name, set_type='train')
+                # write testing set
+                source_factors = create_source_factors(test_set)
+                source, target = convert_to_strings(test_set)
+                create_files(source, target, source_factors, file_dir=file_name, set_type='test')
+                # compute mse using imputation methods and save file
+                scores = [f'{name}, {miss_type}, {miss_percent}', sklearn_impute(training_set, test_set, name=name)]
+                imputation_scores.append(scores)
+                #imputation_scores.to_pickle(Path(file_name.__str__() + 'imputation_score.pkl'))
+                print(imputation_scores)
     # create quadratic and linear numerical values with different noise factors
     for target_type in parameters_syth_data['type']:
         print(target_type)
         dataset = create_numerical_data(target_type=target_type)
         for miss_type in missingness['missingness_type']:
             print(miss_type)
-            mask = generate_missing_mask(dataset, missingness=miss_type)
-            training_set, test_set = generate_missingness(dataset, mask)
-            # write training set
-            source_factors = create_source_factors(training_set)
-            source, target = convert_to_strings(training_set)
-            source, target, source_factors = shuffle_data(source, target, source_factors)
-            #file_name = str(target_type + '/' + miss_type)
-            file_name = Path('data/' + str(target_type + '/' + miss_type))
-            create_files(source, target, source_factors, file_dir=file_name, set_type='train')
-            # write testing set
-            source_factors = create_source_factors(test_set)
-            source, target = convert_to_strings(test_set)
-            create_files(source, target, source_factors, file_dir=file_name, set_type='test')
-            # compute mse using imputation methods and save file          
-            imputation_scores = sklearn_impute(training_set, test_set)
-            imputation_scores.to_pickle(Path(file_name.__str__() + 'imputation_score.pkl'))
+            for miss_percent in missingness['missingness_percent']:
+                print(miss_percent)
+                mask = generate_missing_mask(dataset, missingness=miss_type)
+                training_set, test_set = generate_missingness(dataset, mask)
+                # write training set
+                source_factors = create_source_factors(training_set)
+                source, target = convert_to_strings(training_set)
+                source, target, source_factors = shuffle_data(source, target, source_factors)
+                #file_name = str(target_type + '/' + miss_type)
+                file_name = Path('data/' + f'{target_type}/{miss_type}/{miss_percent}')
+                create_files(source, target, source_factors, file_dir=file_name, set_type='train')
+                # write testing set
+                source_factors = create_source_factors(test_set)
+                source, target = convert_to_strings(test_set)
+                create_files(source, target, source_factors, file_dir=file_name, set_type='test')
+                # compute mse using imputation methods and save file          
+                scores = [f'{target_type}, {miss_type}, {miss_percent}', sklearn_impute(training_set, test_set, name=name)]
+                imputation_scores.append(scores)
+                #imputation_scores.to_pickle(Path(file_name.__str__() + 'imputation_score.pkl'))
+                print(imputation_scores)
+    return imputation_scores
 
 dataset_dict = {
     'boston_data': load_boston, 
@@ -113,7 +123,7 @@ dataset_dict = {
 
 parameters_syth_data = {
     'type': ['quadratic', 'linear'],
-    'amount': [10, 100, 1000, 10000],
+    'amount': [100, 1000, 10000],
     'noise': [0.001, 0.01, 0.1, 1]
     }
 
@@ -121,7 +131,7 @@ missingness = {
     'missingness_type': ['MCAR', 'MAR', 'MNAR'],
     'missingness_percent': [10, 30]}
 
-data_setup()
+test = data_setup()
 # noise_list = [0.001, 0.01, 0.1, 1]
 # for noise in noise_list:
 #     source, target, source_factors = create_numerical_data(noise=noise)
