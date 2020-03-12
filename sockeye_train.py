1ce6b7c4629c8d14d9a307e0d1a31778039279b2
agademic <33003249+agademic@users.noreply.github.com>
2020-03-12 21:07:13 +0100

    Update sockeye_train.py

--- sockeye_train.py
+++ sockeye_train.py
@@ -22,7 +22,7 @@ for dataset in datasets:
             for miss in missingness:
                 print(miss)
                 try:
-                    command_prepr = f'python -m sockeye.train -d {dataset}/{typ}/{miss}/prepared_sockeye_data/ -vs {dataset}/{typ}/{miss}/dev.source -vt {dataset}/{typ}/{miss}/dev.target -vsf {dataset}/{typ}/{miss}/dev.source_factors --optimized-metrics accuracy --metrics accuracy --batch-type sentence --max-num-checkpoint-not-improved 3 -o {dataset}/{typ}/{miss}//sockeye_model_xxl'
+                    command_prepr = f'python -m sockeye.train -d {dataset}/{typ}/{miss}/prepared_sockeye_data/ -vs {dataset}/{typ}/{miss}/dev.source -vt {dataset}/{typ}/{miss}/dev.target -vsf {dataset}/{typ}/{miss}/dev.source_factors --optimized-metric accuracy --metrics accuracy --batch-type sentence --max-num-checkpoint-not-improved 3 -o {dataset}/{typ}/{miss}//sockeye_model_xxl'
                     print(command_prepr)
                     process = subprocess.Popen(command_prepr.split(), stdout=subprocess.PIPE)
                     output, error = process.communicate()
